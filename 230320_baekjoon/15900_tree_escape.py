e82ab9f986313693a317338ab179035f81b2a952
ian <passionsoft.ian@gmail.com>
2023-03-21 18:01:15 +0900

    baekjoon tree |230321

--- 230320_baekjoon/15900_tree_escape.py
+++ 230320_baekjoon/15900_tree_escape.py
@@ -1,54 +1,34 @@
 import sys
 sys.stdin = open('15900_tree_escape_input.txt')
 
-def dfs(stack):
-    global count
-    node = stack
-    s = node.pop()
-    visited[s] = 1
-    for i in edge[s]:
-        if visited[i] == 1:
-            continue
-        if visited[i] == 0:
-            visited[i] = 1
-            dfs([edge[s][i]])
-            return
-    if node == 1:
-        count += 1
-        return
-    if 1 in edge[s]:
-        count += 1
-        return
-    else:
-        for u in range(len(edge[s])):
-            count += 1
-            dfs([edge[s][u]])
-            return
-
-
 N = int(input())
 V = N-1
 edge = [[]*N for i in range(N+1)]
-visited = [0] * (N+1)
+visited = [-1] * (N+1)
+visited[1] = 0
+
 count = 0
 for i in range(V):
     n, m = map(int, input().split())
     edge[n].append(m)
     edge[m].append(n)
-print(edge)
 
 
-stack = []
-for i in range(1, N+1):
-    for j in range(len(edge[i])):
-        if edge[i][j] != 1 and visited[edge[i][j]] == 0:
-            visited[edge[i][j]] += 1
-            stack.append(edge[i][j])
-            dfs(stack)
+
+stack = [1]
+while stack:
+    r = stack.pop()
+    check = True
+    for next_node in edge[r]:
+        if visited[next_node] == -1:
+            visited[next_node] = visited[r] + 1
+            stack.append(next_node)
+            check = False
+
+    if check is True:
+        count += visited[r]
 
 if count % 2 == 0:
     print('No')
-    print(count)
 else:
-    print('Yes')
-    print(count)
+    print('yes')
