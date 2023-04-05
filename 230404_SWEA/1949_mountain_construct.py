5b8d2d647b3322d6062251292f6a7f4249ea725b
Ian <passionsoft.ian@gmail.com>
2023-04-05 17:58:22 +0900

    algo |230405

--- 230404_SWEA/1949_mountain_construct.py
+++ 230404_SWEA/1949_mountain_construct.py
@@ -1,44 +1,45 @@
 import sys
 sys.stdin = open('1949_mountain_construct_input.txt')
-from collections import deque
 
 
 dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
 
 
-def find(q):
-    global count
-    queue = q
-    visited = [[0]*N for _ in range(N)]
-    while queue:
-        n, m = queue.popleft()
-        for k in range(4):
-            ny, nx = n + dy[k], m + dx[k]
-            if 0 <= ny < N and 0 <= nx < N and visited[ny][nx] == 0:
-                if arr[ny][nx] < arr[n][m]:
-                    queue.append([ny, nx])
-                    count += 1
-                    visited[ny][nx] = 1
+def check(stk):
+    global max_result, result
 
+    stack = [stk]
+
+    n, m = stack.pop()
+    for k in range(4):
+        ny, nx = n + dy[k], m + dx[k]
+        if 0 <= ny < N and 0 <= nx < N:
+            if arr[n][m] > arr[ny][nx]:
+                check([ny, nx])
+            if arr[n][m] < arr[ny][nx]:
+                if arr[ny][nx] - K < arr[n][m]:
+                    continue
                 else:
-                    for work in range(1, K+1):
-                        arr[ny][nx] -= work
-                        if arr[ny][nx] < arr[n][m]:
-                            construct([ny, nx], work, ny, nx)
-                        else:
-                            arr[ny][nx] += work
+                    arr[ny][nx] -= arr[ny][nx] - arr[n][m] - 1
+                    result += construct([ny, nx])
+                    arr[ny][nx] += arr[ny][nx] - arr[n][m] - 1
+    if max_result < result:
+        max_result = result
 
 
-def construct(coordi, wrk, by, bx):
-    global count
-    count += 1
-    operate = coordi
-    while operate:
-        n, m = operate.popleft()
-        if arr[n][m]
-
-    arr[by][bx] += wrk
+def construct(stk_c):
+    result_c = 1
+    stack_c = [stk_c]
+    while stack_c:
+        nc, mc = stack_c.pop()
+        for c in range(4):
+            cy, cx = nc + dy[c], mc + dx[c]
+            if 0 <= cy < N and 0 <= cx < N:
+                if arr[nc][mc] > arr[cy][cx] and visited[cy][cx] == 0:
+                    visited[cy][cx] = 1
+                    result_c += 1
+    return result_c
 
 
 Test_case = int(input())
@@ -50,12 +51,12 @@ for tc in range(1, Test_case+1):
 
     hight = []
 
-    coordination = deque()
-    coordination_work = deque()
+    coordination = []
+    coordination_work = []
 
-    construction = deque()
+    construction = []
 
-    count = 0
+    result = 0
 
     max_result = 0
 
@@ -64,9 +65,11 @@ for tc in range(1, Test_case+1):
 
     for hgt in range(N*N):
         if hight[hgt] == max(hight):
-            coordination.append([hgt//N, hgt % N])
-            # coordination_work.append([hgt//N, hgt % N])
+            coordination.append([hgt//N, hgt % N, 0])
 
-    find(coordination)
+    for cd in coordination:
+        visited = [[0] * N for _ in range(N)]
+        visited[cd[0]][cd[1]] = 1
+        check(cd)
 
     print(max_result)
