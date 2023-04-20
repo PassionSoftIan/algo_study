af9de40de0d1c88b7af9c22d1c441fa9ff413eb2
Ian <passionsoft.ian@gmail.com>
2023-04-20 17:57:00 +0900

    bomberman |230420

--- 230419_baekjoon/16918_bomber_man.py
+++ 230419_baekjoon/16918_bomber_man.py
@@ -1,82 +1,48 @@
 import sys
 sys.stdin = open('16918_bomber_man_input.txt')
 
-
 dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
 
 
 N, M, T = map(int, input().split())
 
-arr_1 = [list(map(str, input())) for _ in range(N)]
+arr = [list(map(str, input())) for _ in range(N)]
 
-arr_2 = [['O']*M for _ in range(N)]
+bomb = []
 
-arr_3 = [['O']*M for _ in range(N)]
+launch = []
 
-check = [['.']*M for _ in range(N)]
+count = 1
 
-coordinate = []
+if T == 0 or T == 1:
+    for i in arr:
+        print(*i, sep='')
+    exit()
 
-for i in range(N):
-    for j in range(M):
-        if arr_1[i][j] == 'O':
-            coordinate.append([i, j])
-
-while coordinate:
-    n, m = coordinate.pop()
-    arr_3[n][m] = '.'
-    for k in range(4):
-        ny, nx = n + dy[k], m + dx[k]
-        if 0 <= ny < N and 0 <= nx < M:
-            arr_3[ny][nx] = '.'
-
-if arr_3 == check:
-    if T > 3:
-        if T % 4 == 0:
-            for i in arr_2:
-                print(*i, sep='')
-            exit()
-        else:
-            for i in arr_3:
-                print(*i, sep='')
-            exit()
-
-
-    if T == 0 or T == 1:
-        for i in arr_1:
+while True:
+    if count == T+1:
+        for i in arr:
             print(*i, sep='')
-        exit()
+        break
 
-    if T == 2:
-        for i in arr_2:
-            print(*i, sep='')
-        exit()
+    if count % 2 == 0:
+        for i in range(N):
+            for j in range(M):
+                if arr[i][j] == 'O':
+                    bomb.append([i, j])
+                if arr[i][j] == '.':
+                    launch.append([i, j])
+        while launch:
+            n, m = launch.pop()
+            arr[n][m] = 'O'
 
-    if T == 3:
-        for i in arr_3:
-            print(*i, sep='')
-        exit()
-
-
-
-else:
-    if T == 0 or T == 1:
-        for i in arr_1:
-            print(*i, sep='')
-        exit()
-
-    if T % 2 == 0:
-        for i in arr_2:
-            print(*i, sep='')
-        exit()
-
-    else:
-        if (T // 2) % 2 == 1:
-            for i in arr_3:
-                print(*i, sep='')
-            exit()
-
-        if (T // 2) % 2 == 0:
-            for i in arr_1:
-                print(*i, sep='')
+    if count % 2 == 1:
+        while bomb:
+            n, m = bomb.pop()
+            arr[n][m] = '.'
+            for k in range(4):
+                ny, nx = n + dy[k], m + dx[k]
+                if 0 <= ny < N and 0 <= nx < M:
+                    arr[ny][nx] = '.'
+    count += 1
