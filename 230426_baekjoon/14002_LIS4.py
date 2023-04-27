e570dc465e53c0793bc7c7f779737f1b864fbb2a
Ian <passionsoft.ian@gmail.com>
2023-04-27 18:00:21 +0900

    algo |230427

--- 230426_baekjoon/14002_LIS4.py
+++ 230426_baekjoon/14002_LIS4.py
@@ -4,10 +4,33 @@ sys.stdin = open('14002_LIS4_input.txt')
 N = int(input())
 arr = list(map(int, input().split()))
 dp = [1] * N
-
+lst = []
+result = 0
 for i in range(1, N):
+    temp = []
+    rlt = 0
     for j in range(i):
         if arr[j] < arr[i]:
-            dp[i] = max(dp[i], dp[j]+1)
+            if dp[i] <= dp[j]+1:
+                if dp[i] == dp[j]+1 and temp[-1] > arr[j]:
+                    temp.pop()
+                    temp.append(arr[j])
+                    rlt = max(dp[i], rlt)
+                    print(f'{i} {j}', dp, dp[i], arr[i], arr[j])
+                else:
+                    dp[i] = dp[j]+1
+                    rlt = max(dp[i], rlt)
+                    temp.append(arr[j])
+                    print(f'{i} {j}', dp, dp[i], arr[i], arr[j])
+
+    temp.append(arr[i])
+    if rlt > result:
+        result = rlt
+        lst = temp
 
 print(max(dp))
+
+if not lst:
+    print(arr[0])
+else:
+    print(*lst)
--- 230426_baekjoon/17086_baby_shark.py
+++ 230426_baekjoon/17086_baby_shark.py
@@ -1,35 +1,57 @@
 import sys
 sys.stdin = open('17086_baby_shark_input.txt')
+from collections import deque
+
 
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
 dx = [1, 1, 0, -1, -1, -1, 0, 1]
 
 
 def check(q):
-    q = q
+    visited = [[0]*M for _ in range(N)]
+    count = 0
+    q = deque([q])
     while q:
-        n, m, s = q.pop(0)
+        if count == N*M-1:
+            return
+        n, m, s = q.popleft()
         for k in range(8):
             ny, nx = n + dy[k], m + dx[k]
             if 0 <= ny < N and 0 <= nx < M:
-                if visited[ny][nx] == 0:
-                    visited[ny][nx] = s
+                if visited[ny][nx] == 0 and arr[ny][nx] == 0:
+                    visited[ny][nx] = 1
+                    count += 1
                     q.append([ny, nx, s+1])
-
-
+                    if info[ny][nx] > s:
+                        info[ny][nx] = s
+                if arr[ny][nx] == 1 and info[ny][nx] == 5001:
+                    count += 1
 
 
 N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-arr = [list(map(int, input().split()))]
+info = [[5001]*M for _ in range(N)]
 
-visited = [[0]*M for _ in range(N)]
+bit = 0
 
-stack = []
+result = 0
 
 for i in range(N):
     for j in range(M):
         if arr[i][j] == 1:
-            check([i, j, 0])
+            info[i][j] = 1
+            check([i, j, 1])
+            bit = 1
 
-print(visited)
+if bit == 0:
+    print(0)
+    exit()
+
+for i in info:
+    result = max(result, max(i))
+
+for i in info:
+    print(i)
+
+print(result)
